4f2859a8117c742de176db520826cdd9fd7504fa
Jpusername1 <royamsemaj@gmail.com>
2022-12-18 23:02:00 +0800

    First Commit

---
+++ public/js/reservation.js
@@ -0,0 +1,23 @@
+
+
+////////////////////////////////FOR SLIDER IMAGES
+const swiper = new Swiper('.swiper', {
+
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+    
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        },
+    
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+    
+        
+    });
