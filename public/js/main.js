58950ddedf341f359d70cd1408481bca30c4c72a
James Phillip Mayor <117265110+JpMayor1@users.noreply.github.com>
2022-12-20 18:30:18 +0800

    Third Commit
    FOODS AND DRINKS

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,26 @@
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
 
-menu.onclick = () =>{
+menu.onclick = () => {
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+};
 
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+window.onscroll = () => {
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+};
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+});
